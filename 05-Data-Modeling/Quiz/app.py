a17e6ea04f246787c163510ba78f2a9f122958f0
Patine-178 <77371883+Patine@users.noreply.github.com>
2021-02-05 10:02:41 +0700

    Homework-2

---
+++ 05-Data-Modeling/Quiz/app.py
@@ -0,0 +1,65 @@
+from flask import Flask,render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from models import db, Books
+
+ 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+db.init_app(app) 
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+
+@app.route('/')
+def home():
+    books = Books.query.all()
+    return render_template('home.html', books = books)
+
+@app.route('/insert', methods=["GET", "POST"])
+def insert():
+    if request.method == 'GET':
+        return render_template("insert.html")
+
+    if request.method == 'POST':
+        title = request.form['title']
+        author = request.form['author']
+        genre = request.form['genre']
+        height = request.form['height']
+        publisher = request.form['publisher']
+        book = Books(title=title, author=author, genre=genre, height=height, publisher=publisher)
+        db.session.add(book)
+        db.session.commit()
+        return redirect('/')
+
+@app.route('/update/<string:title>', methods=["GET", "POST"])
+def update(title):
+    book = Books.query.filter_by(title=title).first()
+    if request.method == 'POST':
+        if book:
+            db.session.delete(book)
+            db.session.commit()
+ 
+            title = request.form['title']
+            author = request.form['author']
+            genre = request.form['genre']
+            height = request.form['height']
+            publisher = request.form['publisher']
+            book = Books(title=title, author=author, genre=genre, height=height, publisher=publisher)
+ 
+            db.session.add(book)
+            db.session.commit()
+            return redirect('/')
+
+    return render_template("update.html", book=book)
+
+@app.route('/delete/<string:title>', methods=["GET", "POST"])
+def delete(title):
+    book = Books.query.filter_by(title=title).first()
+    if request.method == 'GET':
+        if book:
+            db.session.delete(book)
+            db.session.commit()
+            return redirect('/')
---
+++ 05-Data-Modeling/app1/myapp.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db' # บอก database ที่จะใช้ (SQLite และ ชื่อ database)
+db = SQLAlchemy(app) # ตัวจัดการ database ทั้งหมด
+
+# User table
+class User(db.Model): # สืบทอด db.Model (ถ้าต้องให้ class เป็นตาราง ต้องสืบทอด db.Model)
+    id = db.Column(db.Integer, primary_key=True) # สร้าง column ชือ id เป็น ini และ เป็น primary key
+    username = db.Column(db.String(80), unique=True, nullable=False) # เป็น string ห้ามเป็น null
+    email = db.Column(db.String(120), unique=True, nullable=False) # เป็น string ห้ามเป็น null
+
+
+
+
+
