7d4f9616a587629fc8042b5adf26c89b83d6f6b6
Patine-178 <77371883+Patine@users.noreply.github.com>
2021-03-11 16:46:04 +0700

    Project

---
+++ Project/app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from models import db, Students
+from blueprints.home import home
+from blueprints.dev import dev_name
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.register_blueprint(home)
+app.register_blueprint(dev_name)
+
+db.init_app(app)
+
+@app.before_first_request
+def create_table():
+    db.create_all()
